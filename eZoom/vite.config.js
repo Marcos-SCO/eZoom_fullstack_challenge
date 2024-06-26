aa44b6a8b7b1ffccf5367df39653f1c73f18c155
Marcos dos Santos Carvalho <marcos_sco@outlook.com>
2024-06-26 01:58:34 -0300

    build: vite alias

--- eZoom/vite.config.js
+++ eZoom/vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
+import path from 'path';
+
 export default defineConfig({
     plugins: [
         laravel({
@@ -8,9 +10,15 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    server: { 
+    server: {
         hmr: {
             host: 'http://localhost:8091',
         },
-    }, 
+    },
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname),
+            '@resources': path.resolve(__dirname, 'resources'),
+        }
+    }
 });
